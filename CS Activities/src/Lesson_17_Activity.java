6510b6bf15f775bc2a99131fedd26e9c494ab570
Stephen <Stephen@192.168.0.30>
2017-04-16 15:22:24 -0700

    Updating Lesson 17

--- CS Activities/src/Lesson_17_Activity.java
+++ CS Activities/src/Lesson_17_Activity.java
@@ -1,24 +1,25 @@
+import java.util.*;
 
-public class Lesson_17_Activity {
-	
+public class Lesson_17_Activity
+{
 	public static void main(String[] args)
 	{
 		int[] test;
-		test = new int[]{1, 2, 3, 4, 5};
-		//System.out.println("answer is " + isSorted(test));
-		System.out.println(binarySearch(test, 2));
+		test = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 10};
+		// System.out.println("answer is " + isSorted(test));
+		System.out.println(binarySearch(test, 8));
 	}
-
+	
 	public static boolean isSorted(int[] a)
 	{
 		boolean flag = true;
-		for(int i = 0; i<a.length-1; i++)
+		for(int i = 0; i < a.length - 1; i++ )
 		{
-			for(int j = i+1; j < a.length; j++)
+			for(int j = i + 1; j < a.length; j++ )
 			{
-				//System.out.println(a[i] + " VS. " + a[j]);
-				flag = (a[i]>a[j])? false : true;
-				//System.out.println(flag);
+				// System.out.println(a[i] + " VS. " + a[j]);
+				flag = (a[i] > a[j])? false: true;
+				// System.out.println(flag);
 				if(flag == false)
 				{
 					return flag;
@@ -27,57 +28,44 @@ public class Lesson_17_Activity {
 		}
 		return flag;
 	}
-
+	
 	public static int binarySearch(int[] a, int x)
 	{
-		int min = 0;
-		int max = a.length-1;
-		int mid = (a.length-1)/2;
-		int[] recursion = new int[(a.length-1)/2];
+		int answer = -1;
+		
 		if(isSorted(a) == true)
 		{
-			if(x == a[mid])
-			{
-				return mid;
-			}
-			else if(x == a[max])
-			{
-				return max;
-			}
-			else if(x == a[min])
-			{
-				return min;
-			}
-			else if(x > a[mid] && x < a[max])
-			{
-				for(int i = mid; i < max; i++)
-				{
-					recursion[i] = a[i];
-				}
-				binarySearch(recursion, x);
-			}
-			else if(x < a[mid] && x > a[min])
-			{
-				for(int i = min; i < mid; i++)
-				{
-					recursion[i] = a[i];
-				}
-				binarySearch(recursion, x);
-			}
-			else
-			{
-				return -1;
-			}
+			int length = a.length;
+			int half = (a.length + a.length % 2) / 2;
+			int minimum = a[0];
+			int minIndex = 0;
+			int maximum = a[length-1];
+			int maxIndex = length-1;
+			int median = a[length/2];
+			int medIndex = length/2;
 			
-		}
-		else
-		{
-			for(int i = 0; i < a.length; i++)
+			System.out.println("minimum: " + minimum);
+			System.out.println("median: " + median);
+			System.out.println("maximum: " + maximum);
+			System.out.println("length: " + length);
+			System.out.println("half: " + half);
+			
+			if(x == maximum)
+				answer = maxIndex;
+			if(x == minimum)
+				answer = minIndex;
+			if(x == median)
+				answer = medIndex;
+			if(x < maximum && x > median)
 			{
-				if(a[i] == x)
-					return i;
+				
+			}
+			if(x < median && x > minimum)
+			{
+				
 			}
 		}
-		return -1;
+		
+		return answer;
 	}
 }
