bd25ca6ad23c1bd62059b43cfca177d6d244c5c2
student <student@10.90.40.54>
2017-05-05 14:10:31 -0700

    Initial Commit

---
+++ Hangman/src/src/Hangman.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Hangman
+{
+	private char[] word;
+	private boolean[] verify;
+	
+	Hangman(String setup)
+	{
+		
+	}
+	
+	private char[] convert(String input)
+	{
+		char[] answer = new char[input.length()-1];
+		
+		return answer;
+	}
+	
+}
---
+++ Hangman/src/src/Play_Hangman.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.*;
+
+public class Play_Hangman
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("////////////////////////////// \tWelcome to Hangman!\t\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		System.out.println("\n\n Player 2 look away!\n Player 1 enter your word or phrase.");
+		String answer = scan.nextLine();
+		System.out.println("\n\n\n\n\n\n\n\n\n\nDo Not Scroll Up! No Cheating!");
+		
+	}
+
+}
